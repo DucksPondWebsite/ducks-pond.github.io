b92935573262e3fc15d043182f2615a53399dca6
DucksPondWebsite <duckbydaylight@gmail.com>
2024-12-15 21:05:33 -0600

    Create style.css

---
+++ style.css
@@ -0,0 +1,29 @@
+const quotes = [
+    "What is a... Ducks Pond? - Teachers",
+    "When is the official version of Ducks Pond coming out? - literally everyone (2024)",
+    "hey look at this *aggressively points to computer screen*. - Dante",
+    "Guess what cologne I'm wearing. - Dante",
+    "Do your fucking work! - Me",
+    "Urgh, wat da fuk - Dalton Trey",
+    "ah! my ears burn..",
+    "It's time to have some fun... With Uncle Samsonite",
+    "If Silas Randolph sees this... COMB YOUR FUCKING HAIR!!!",
+    "I will not discriminate with any other game website",
+    "Alt links posted soon",
+    "SHUT UP ABOUT FOOTBALL BROS, I JUST WANT PEACE",
+    "Come here big boy~",
+    "Mikael Myer",
+    "Boo? Whachu lookin at?! - Carlotta",
+    "HEY HEy, hEy, hey... guess what? I'm gonna touch you.",
+    "Me when, whe- me when th- the.. me- whe- the me- me when the- Yeah!"
+];
+
+function getRandomQuote() {
+    const randomIndex = Math.floor(Math.random() * quotes.length);
+    return quotes[randomIndex];
+}
+
+const quote = getRandomQuote(); // Get a random quote
+
+// Update header text
+document.querySelector('#quote').innerHTML = quote;
